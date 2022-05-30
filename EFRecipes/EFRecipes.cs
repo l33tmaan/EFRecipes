223a4268753b13d0df151d08d910e890b845ad38
VinterNacht <crannnabeatha@gmail.com>
2022-05-30 10:48:53 -0500

    Add project files.

---
+++ EFRecipes/EFRecipes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Vintagestory.API.Common;
+
+namespace EFRecipes
+{
+    public class EFRecipes : ModSystem
+    {
+        public class ACulinaryArtillery : ModSystem
+        {
+            public override void Start(ICoreAPI api)
+            {
+                base.Start(api);
+            }
+        }
+
+    }
+}
