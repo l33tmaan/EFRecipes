cfd11747eaa40d4acd4a67993b8e139b03a3d1bb
VinterNacht <crannnabeatha@gmail.com>
2022-06-11 11:18:30 -0500

    1.16_v1.0.0 -> Release?

--- EFRecipes/EFRecipes.cs
+++ EFRecipes/EFRecipes.cs
@@ -5,13 +5,9 @@ namespace EFRecipes
 {
     public class EFRecipes : ModSystem
     {
-        public class ACulinaryArtillery : ModSystem
-        {
             public override void Start(ICoreAPI api)
             {
                 base.Start(api);
             }
-        }
-
     }
 }
