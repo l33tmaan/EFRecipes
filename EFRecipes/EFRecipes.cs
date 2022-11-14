e73902661700764b5cce57f52f870e9788b5c4f8
l33tmaan <tyler.patin@gmail.com>
2022-11-14 10:55:05 -0800

    Added Compote; Lard transition timer reduced from 1440 hours to 240

--- EFRecipes/EFRecipes.cs
+++ EFRecipes/EFRecipes.cs
@@ -1,14 +1,26 @@
-﻿using EFRecipes;
+﻿using ACulinaryArtillery;
+using ACulinaryArtillery.Util;
 using System;
 using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
 
 namespace EFRecipes
 {
     public class EFRecipes : ModSystem
     {
             public override void Start(ICoreAPI api)
-            {
-                base.Start(api);
-            }
-    }
+		{
+			base.Start(api);
+
+			CookingRecipe.NamingRegistry["compote"] = new acaRecipeNames();
+            CookingRecipe.NamingRegistry["augratin"] = new acaRecipeNames();
+            CookingRecipe.NamingRegistry["riceandbeans"] = new acaRecipeNames();
+            CookingRecipe.NamingRegistry["meatysalad"] = new acaRecipeNames();
+            CookingRecipe.NamingRegistry["yogurtmeal"] = new acaRecipeNames();
+            CookingRecipe.NamingRegistry["pastahot"] = new acaRecipeNames();
+            CookingRecipe.NamingRegistry["pastacold"] = new acaRecipeNames();
+		}
+	}
 }
