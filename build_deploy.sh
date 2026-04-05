5d2de19b70873ae93a4645b4cb674c698dc6fc0a
Lasersquid <lasersquid@steamdeck>
2026-04-04 18:38:52 -0600

    Silence many warnings

--- build_deploy.sh
+++
@@ -1,3 +0,0 @@
-zipname=ExpandedFoods_net10.zip
-
-dotnet build && pushd EFRecipes/bin/Debug/Mods/mod/net10.0/ && rm -f $zipname && zip -r $zipname * && cp $zipname ~/vsmod-packages/ && cp ~/vsmod-packages/$zipname ~/.config/VintagestoryData/Mods/ && popd
